ceafaeb8f0b8f3036a9c02b0af3037cac03139ab
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-08 11:45:54 +0500

    Add files via upload

---
+++ Day 7/7.js
@@ -0,0 +1,13 @@
+//Q:19 Dinner Guests
+var guest_dinner = ["Talha", "Daniyal", "Amir", "Sarfaraz"];
+console.log("I invited ".concat(guest_dinner.length, " people to dinner."));
+//Q:20 
+var fav_countries = ["Switzerland", "Japan", "Russia", "Norway"];
+console.log("I would like to visit: ".concat(fav_countries));
+//Q:21
+var car = {
+    model: "lx570",
+    Company: "Lexus",
+    yearofimoprt: 2017
+};
+console.log("Car information ".concat(car.model, " by ").concat(car.Company, " import in ").concat(car.yearofimoprt));
---
+++ Day 7/7.ts
@@ -0,0 +1,15 @@
+//Q:19 Dinner Guests
+let guest_dinner: string[] = ["Talha", "Daniyal", "Amir", "Sarfaraz"]
+console.log(`I invited ${guest_dinner.length} people to dinner.`)
+
+//Q:20 
+let fav_countries: string[] = ["Switzerland", "Japan", "Russia", "Norway"]
+console.log(`I would like to visit: ${fav_countries}` )
+
+//Q:21
+let car: {model: string; Company: string; yearofimoprt: number} = {
+    model: "lx570",
+    Company: "Lexus",
+    yearofimoprt: 2017
+}
+console.log(`Car information: ${car.model} by ${car.Company} import in ${car.yearofimoprt}`)
