de685609095b99b3d4db12c58697e5857ebf5a19
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-23 11:19:33 +0500

    Add files via upload

---
+++ Day 22/index.js
@@ -0,0 +1,15 @@
+//Q:64 combine string
+function combine(age, number) {
+    return age + number;
+}
+console.log(combine("age: ", 30));
+//Q:65 Determine the remainder
+function det_num(a, b) {
+    return a % b;
+}
+console.log(det_num(5, 2));
+//Q:66 Logical and verification
+function check_num(x, y) {
+    return x && y;
+}
+console.log(check_num(true, false));
---
+++ Day 22/index.ts
@@ -0,0 +1,18 @@
+//Q:64 combine string
+function combine(age: string, number: number): string {
+    return age + number
+}
+console.log(combine("age: ", 30));
+
+//Q:65 Determine the remainder
+function det_num(a: number, b: number): number {
+    return a % b 
+}
+console.log(det_num(5, 2));
+
+//Q:66 Logical and verification
+function check_num (x: boolean, y: boolean): boolean {
+    return x && y
+}
+console.log(check_num(true, false));
+
