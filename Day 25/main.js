84884f0f76cb09b696695806cbf6bca7bdd00225
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-26 20:33:10 +0500

    Add files via upload

---
+++ Day 25/main.js
@@ -0,0 +1,30 @@
+//Q:73
+function re_var() {
+    var names = "Shafique";
+    console.log(names);
+    names = "Khanzada";
+    console.log("Updated value: ", names);
+}
+re_var();
+//Q:74
+var a = 5;
+var b = 10;
+var temp = a;
+a = b;
+b = temp;
+console.log("a: ", a);
+console.log("b: ", b);
+//Q:75
+function compund() {
+    var x = 4;
+    console.log(4);
+    x += 1;
+    console.log(x);
+    x -= 2;
+    console.log(x);
+    x *= 7;
+    console.log(x);
+    x /= 2;
+    console.log(x);
+}
+compund();
---
+++ Day 25/main.ts
@@ -0,0 +1,36 @@
+//Q:73
+function re_var () {
+    let names = "Shafique"
+    console.log(names);
+
+    names = "Khanzada"
+    console.log("Updated value: ", names);    
+}
+re_var()
+
+//Q:74
+let a = 5
+let b = 10
+
+let temp = a
+a = b
+b = temp
+console.log("a: ", a);
+console.log("b: ", b);
+
+//Q:75
+let x = 4
+console.log(4);
+
+x += 1
+console.log(x); 
+x -= 2
+console.log(x);
+x *= 7
+console.log(x);
+x /= 2
+console.log(x);
+ 
+
+
+
