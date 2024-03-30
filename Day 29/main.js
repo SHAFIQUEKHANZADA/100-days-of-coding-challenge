31c4cede9cf41d11375f114e244ad20a2f57a1cd
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-03-30 10:54:41 +0500

    Add files via upload

---
+++ Day 29/main.js
@@ -0,0 +1,15 @@
+//Q:85
+function a(x) {
+    return x.indexOf("code");
+}
+console.log(a("Learn code with typescript"));
+//Q:86
+function check(text) {
+    return text.includes("typescript");
+}
+console.log(check("Hello, i am learnin typescript"));
+//Q:87
+function abs(c) {
+    return c.substring(0, 10);
+}
+console.log(abs("Hello, my name is shafique ur rehman and i am the student of GIAIC"));
---
+++ Day 29/main.ts
@@ -0,0 +1,17 @@
+//Q:85
+function a (x : string):number {
+    return x.indexOf("code")
+}
+console.log(a("Learn code with typescript"));
+
+//Q:86
+function check (text: string): boolean {
+    return text.includes("typescript")
+}
+console.log(check("Hello, i am learnin typescript"));
+
+//Q:87
+function abs (c: string): string {
+    return c.substring(0, 10)
+} 
+console.log(abs("Hello, my name is shafique ur rehman and i am the student of GIAIC"));
