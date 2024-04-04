0b32279c05d2c1b5f2df0fe13609db227c8ae230
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-04-04 09:44:20 +0500

    Add files via upload

---
+++ Day 34/main.js
@@ -0,0 +1,9 @@
+//Q:100
+var findRoot = Math.sqrt(144);
+console.log(findRoot);
+//Q:101
+var renDom = Math.ceil(Math.random() * 10) + 1;
+console.log(renDom);
+//Q:102
+var abs = Math.abs(-5 - 10);
+console.log(abs);
---
+++ Day 34/main.ts
@@ -0,0 +1,11 @@
+//Q:100
+let findRoot = Math.sqrt(144)
+console.log(findRoot)
+
+//Q:101
+let renDom = Math.ceil(Math.random() * 10) + 1
+console.log(renDom)
+
+//Q:102
+let abs = Math.abs(-5 -10)
+console.log(abs)
