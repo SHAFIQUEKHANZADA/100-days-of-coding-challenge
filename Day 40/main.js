43c4ce914fb1fbb23d27b3f105f1fc47064b88d3
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-04-10 12:02:44 +0500

    Add files via upload

---
+++ Day 40/main.js
@@ -0,0 +1,16 @@
+//Q:118
+for (var a = 1; a <= 10; a++) {
+    console.log(a);
+}
+//Q:119
+var b = 0;
+while (b < 5) {
+    console.log("Hello World!");
+    b++;
+}
+//Q:120
+var myFvt = ["Pirates of the Caribbean", "Fast & Furious", "Jumanji The Next Level", "The Old Guard"];
+for (var _i = 0, myFvt_1 = myFvt; _i < myFvt_1.length; _i++) {
+    var my = myFvt_1[_i];
+    console.log("My favorite movie is: ".concat(my));
+}
---
+++ Day 40/main.ts
@@ -0,0 +1,15 @@
+//Q:118
+for(let a = 1; a <= 10; a++) {
+    console.log(a)
+}
+//Q:119
+let b = 0
+while(b < 5) {
+    console.log("Hello World!")
+    b++
+}
+//Q:120
+let myFvt = ["Pirates of the Caribbean", "Fast & Furious", "Jumanji The Next Level", "The Old Guard"]
+for(let my of myFvt){
+    console.log(`My favorite movie is: ${my}`)
+}
