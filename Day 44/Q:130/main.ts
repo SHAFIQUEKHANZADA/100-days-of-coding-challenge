6f88d78876c198c7c2fae5269c2b1b6c603d52c6
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-04-16 09:09:58 +0500

    Add files via upload

---
+++ Day 44/Q:130/main.ts
@@ -0,0 +1,3 @@
+export let fruits = ['apple', 'banana', 'kiwi', 'orange']
+
+//Q:131
---
+++ Day 44/Q:130/main2.ts
@@ -0,0 +1,4 @@
+import { fruits } from "./main";
+
+console.log(fruits.forEach(x => console.log(`I like ${x}`)
+));
