9754a842b96bd2e289daabc57d65aab197e951ad
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-04-17 10:01:37 +0500

    Add files via upload

---
+++ Day-47/main.ts
@@ -0,0 +1,19 @@
+//139
+const num = 10
+
+switch(num) {
+    case 10:
+        console.log("This is number 10")
+    break
+}
+let add = (x:number,y:number):number => {return x + y}
+
+//140
+// let if = "hello"
+let ifx = "hello"
+
+//141
+async function fetchData() {
+    const data = await fetchData();
+    console.log(data);
+}
