0d0eb57129527cba681a5891207766152e344ad3
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-05-16 12:46:40 +0500

    Add files via upload

---
+++ Day-74/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 8));
+console.log("Hello world");
+//# sourceMappingURL=index.js.map
---
+++ Day-74/index.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+    return a + b
+}
+console.log(add(5,8));
+
+console.log("Hello world");
